12634bac4f6ba7868ef31517840591ed8d07513a
adamyordan01 <adamyordan0102@gmail.com>
2024-01-15 19:25:13 +0700

    add produt routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
 
     Route::get('/', function () {
@@ -25,5 +26,14 @@ use Illuminate\Support\Facades\Route;
             Route::post('/get-category-detail', [CategoryController::class, 'getCategoryDetail'])->name('category.getCategoryDetail');
             Route::patch('/update', [CategoryController::class, 'update'])->name('category.update');
             Route::delete('/delete/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
+        });
+
+        // group prefix product
+        Route::prefix('product')->group(function () {
+            Route::get('/', [ProductController::class, 'index'])->name('product.index');
+            Route::post('/', [ProductController::class, 'store'])->name('product.store');
+            Route::post('/get-product-detail', [ProductController::class, 'getProductDetail'])->name('product.getProductDetail');
+            Route::patch('/update', [ProductController::class, 'update'])->name('product.update');
+            Route::delete('/delete/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
         });
     });
